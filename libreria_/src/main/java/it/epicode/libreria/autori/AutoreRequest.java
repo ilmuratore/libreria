df76e1b496dcc21354c844af84d7565223176987
Simone Iengo <iengo.simone@gmail.com>
2024-06-10 16:03:09 +0200

    aggiunti commenti pt2

--- libreria_/src/main/java/it/epicode/libreria/autori/AutoreRequest.java
+++ libreria_/src/main/java/it/epicode/libreria/autori/AutoreRequest.java
@@ -8,3 +8,8 @@ public class AutoreRequest {
     private String cognome;
 
 }
+
+// AutoreRequest
+// Questo oggetto viene utilizzato quando si desidera creare o modificare un autore.
+// Le proprieta' di questo oggetto vengono copiate in un oggetto Autore, che viene successivamente salvato nel DB.
+// AutoreRequest rappresenta il DTO dei dati in entrata che l'utente invia all'applicazione per creare o modificare un autore
--- libreria_/src/main/java/it/epicode/libreria/autori/AutoreResponse.java
+++ libreria_/src/main/java/it/epicode/libreria/autori/AutoreResponse.java
@@ -8,3 +8,11 @@ public class AutoreResponse {
     private String nome;
     private String cognome;
 }
+
+
+// AutoreResponse
+// Questo oggetto viene utilizzato quando si desidera resituire i dati di un'autore all'utente.
+// Quando si recupera un autore dal db, le proprieta' dell'entita autore vengono copiate in un oggetto AutoreRequest e poi restituito all'utente.
+// AutoreResponse rappresenta il DTO dei dati in uscita che l'utente visualizza sulla sua applicazione riguardo i dati di un Utente
+
+
